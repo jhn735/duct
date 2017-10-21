f223c99741015227fb59d9948f00c7e8aa1d8600
John Villa <jhn735@gmail.com>
2017-10-21 13:47:23 -0700

    Added main class for project.

---
+++ src/Duct.java
@@ -0,0 +1,15 @@
+package duct.main;
+import java.lang.System;
+
+public class Duct{
+
+  public Duct(){
+    
+  }
+
+  public static void main(String[] args){
+		System.out.println("hello world!!");
+
+  }
+
+}
