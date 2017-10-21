90dfe7fa6ea0945126444cb5eb927c718aa186fc
John Villa <jhn735@gmail.com>
2017-10-21 15:30:05 -0700

    Finally got the hang of ivy.

--- src/main/Duct.java
+++ src/main/Duct.java
@@ -1,15 +1,32 @@
 package duct.main;
 import java.lang.System;
+import org.apache.commons.lang3.StringUtils;
 
+/**
+ * This class loads everything that needs loading and brings up a repl when not
+ * disabled by user arguments. 
+*/
 public class Duct{
 
   public Duct(){
     
   }
 
-  public static void main(String[] args){
-		System.out.println("hello world!!");
-
+  public Duct(String[] args){
+    parseArgs(args);
   }
 
+  public static void main(String[] args){
+		System.out.println("hello world!!");
+    Duct d = new Duct(args);
+  }
+  
+  private void parseArgs(String[] args){
+    String arg;
+    for(int i = 0; i < args.length; i++){
+			arg = args[i];
+      
+      System.out.println(StringUtils.trimToEmpty(args[i]));
+		}
+  }
 }
