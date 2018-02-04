b1ebeabc91565a0bbb9facaf4aa6bc3af235ab15
John Villa <jhn735@gmail.com>
2018-02-04 14:47:46 -0800

    Added javadoc commentary for functions in Operation interface.

--- src/main/lang/Operation.java
+++ src/main/lang/Operation.java
@@ -6,7 +6,17 @@ import java.util.List;
   * You also want to make sure it has a name.
  **/
 public interface Operation {
+ /**
+   * Retrieves the name of the operation or an empty string if it doesn't have one.
+   * @return A string containing the name of the operation or an empty string if the operation has no name.
+  **/
  public String getName();
+
+ /**
+   * Executes the operation with the given values as parameters.
+   * @param values The list of values to be passed in as parameters.
+   * @return A value which is the result of the operation.
+  **/
  public Value execute(List<Value> values);
 
 }
