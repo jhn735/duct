d705c53e26dc26cc44afd479345a98e2bfad406b
John Villa <jhn735@gmail.com>
2018-02-05 12:07:00 -0800

    Added abstract function settings which returns the current settings of the module to be stored by the executor.

--- src/main/lang/Module.java
+++ src/main/lang/Module.java
@@ -1,6 +1,7 @@
 package duct.main.lang;
 import java.lang.CharSequence;
 import java.util.Map;
+import java.util.HashMap;
 
 /**
  * Making the module an abstract and not an interface so that I can better control
@@ -10,8 +11,31 @@ import java.util.Map;
  * think the project will go.
 **/
 public abstract class Module{
+  //The name of the module which must be set when extending this class.
+  public final String Name;
+
+  /**
+    * Please note that the name must not be null otherwise this will be problem for your module.
+   **/
+  public Module(CharSequence name, Map<String, byte[]> settings){
+    this.Name = name.toString();
+    this.settings = new HashMap<String, byte[]>(settings); 
+  } 
   
+  public Module(CharSequence name){
+   this.Name = name.toString(); 
+   this.settings = new HashMap<String, byte[]>() 
+  }
+
   public List<String> operationNames();
   public Operation getOperation(CharSequence name);
   public Map<String, Operation> operations();
+  
+  /**
+    * A function for the retrieval of the modules settings to be store in long term storage. 
+    * Storing value in bytes for the module to handle for itself. 
+    * Encryption can be done on these values if need be, but the executor may encrypt it anyway.
+    * @return A map between the name of the property and the value to be stored in bytes.
+   **/ 
+  public Map<String, byte[]> settings();   
 }
