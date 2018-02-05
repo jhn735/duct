9da3c73f5bc2a03ee230825c510d3ac729588d8c
John Villa <jhn735@gmail.com>
2018-02-05 12:38:31 -0800

    Removed uneccessary functions and added a default function to store an object as a module property.

--- src/main/lang/Module.java
+++ src/main/lang/Module.java
@@ -2,6 +2,7 @@ package duct.main.lang;
 import java.lang.CharSequence;
 import java.util.Map;
 import java.util.HashMap;
+import java.nio.charset.Charset;
 
 /**
  * Making the module an abstract and not an interface so that I can better control
@@ -11,8 +12,11 @@ import java.util.HashMap;
  * think the project will go.
 **/
 public abstract class Module{
+  private static final Charset DEFAULT_CHARSET = java.nio.charset.StandardCharsets.UTF_8;
   //The name of the module which must be set when extending this class.
   public final String Name;
+
+  protected Map<String, byte[]> settings;
 
   /**
     * Please note that the name must not be null otherwise this will be problem for your module.
@@ -27,9 +31,23 @@ public abstract class Module{
    this.settings = new HashMap<String, byte[]>() 
   }
 
-  public List<String> operationNames();
-  public Operation getOperation(CharSequence name);
+  /**
+    * Retrieves the operations available within this module mapped to their names.
+    * @return A Map between the names of the operations and the operation objects available within this module.
+   **/
   public Map<String, Operation> operations();
+  
+  /**
+    * Stores the given object into the settings map for long term storage.
+    * Unless overriden, it stores the bytes from the results of the object's toString() function with UTF-8 encoding.
+    * @param name The name of the property to store the value under.
+    * @param obj The object representing the value of the property.
+   **/
+  public void storeProperty(CharSequence name, Object obj){
+    if(settings == null)
+      settings = new HashMap<String, byte[]>();
+    settings.put(name.toString(), obj.toString().getBytes(DEFAULT_CHARSET));
+  }  
   
   /**
     * A function for the retrieval of the modules settings to be store in long term storage. 
@@ -37,5 +55,9 @@ public abstract class Module{
     * Encryption can be done on these values if need be, but the executor may encrypt it anyway.
     * @return A map between the name of the property and the value to be stored in bytes.
    **/ 
-  public Map<String, byte[]> settings();   
+  public Map<String, byte[]> settings(){
+    if(settings == null)
+      settings = new HashMap<String, byte[]>();
+  return settings;
+  }   
 }
