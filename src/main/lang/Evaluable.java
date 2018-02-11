e571a138cc8ef82e879491c64049d05649f993b2
John Villa <jhn735@gmail.com>
2018-02-11 13:37:24 -0800

    Made the Evaluable inteface extend callable to make it more multithreading friendly.

--- src/main/lang/Evaluable.java
+++ src/main/lang/Evaluable.java
@@ -1,9 +1,18 @@
 package duct.main.lang;
+import java.util.concurrent.Callable;
 
 /* Each class that is evaluable has the responsibility of defining a way
  * for it to evaluate itself. Context at time of evaluation can only be guaranteed
  * if the implementing class requires it..
+ * I have Evaluable extend callable vs having the other classes implement callable 
+ * directly because:
+ * * It sucks to have to write 'implements Callable<Value>' 
+ * * It's easier and important to differentiate evaluables from simple callables.
+ * * If the interface needs to be changed, it can be changed.  
  */
-public interface Evaluable{
+public interface Evaluable extends Callable<Value>{
   public Value evaluate();
+  default Value call(){
+    return evaluate();
+  }
 }
