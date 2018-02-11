d8e892d2a8a24f4a9f46c725f936e858a942c20d
John Villa <jhn735@gmail.com>
2018-02-11 13:46:56 -0800

    Turned the Operation interface into an abstract class.

--- src/main/lang/Operation.java
+++ src/main/lang/Operation.java
@@ -1,22 +1,24 @@
 package duct.main.lang;
 import java.util.List;
+import java.util.concurrent.Callable;
 /**
   * Don't care much about implementation, only that when you give it a list of 
   * Values and tell it to execute, it does something and returns a value.
   * You also want to make sure it has a name.
  **/
-public interface Operation {
- /**
-   * Retrieves the name of the operation or an empty string if it doesn't have one.
-   * @return A string containing the name of the operation or an empty string if the operation has no name.
-  **/
- public String getName();
+public abstract class Operation { 
+ public final String name;
+ public final Module module;
+
+ public Operation(CharSequence name, Module module){
+   this.name = name.toString();
+   this.module = module;
+ }
 
  /**
    * Executes the operation with the given values as parameters.
-   * @param values The list of values to be passed in as parameters.
+   * @param operands The list of values to be passed in as parameters.
    * @return A value which is the result of the operation.
   **/
- public Value execute(List<Value> values);
-
+ public Value execute(List<Value> operands);
 }
