0248f8ced5a7ffc0fb1ea83b6f630c2e11a29120
John Villa <jhn735@gmail.com>
2018-02-11 15:16:14 -0800

    Created Element class which should work as a standin for functional elements of a script like values, modules, operators, etc.

---
+++ src/main/lang/Element.java
@@ -0,0 +1,14 @@
+package duct.main.lang;
+
+
+/**
+  * A class to represent any functional element in the duct language.
+ **/
+public abstract class Element{
+  public final String name;
+
+  public Element(CharSequence name){
+    this.name = name.toString();
+  }
+
+}
