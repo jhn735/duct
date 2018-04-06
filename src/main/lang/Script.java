f152b23e7584fdc577507f2f597b375fe40ea3e3
John Villa <jhn735@gmail.com>
2018-04-06 16:10:48 -0700

    Wrote code in Script class to parse a script file and at least extract the name.

--- src/main/lang/Script.java
+++ src/main/lang/Script.java
@@ -1,9 +1,17 @@
 package duct.main.lang;
 
+import duct.main.lang.ParseUtils;
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.Reader;
+import java.lang.Character;
 import java.lang.Runnable;
+import java.text.ParseException;
 import java.util.List;
 import java.util.Set;
+import java.util.ArrayList;
+import java.util.HashSet;
+import org.apache.commons.lang3.StringUtils;
 
 /**
   * An object to represent a script, interpreted and packaged to be run whenever.
@@ -13,7 +21,7 @@ public class Script extends Element implements Runnable{
   private final Set<Module> modules;
   private final List<Evaluable> instructions;
 
-  public Script(CharSequence name, List<Evaluable> instructions, Executor exe, Set<Module> modules){ 
+  public Script( CharSequence name, List<Evaluable> instructions, Executor exe, Set<Module> modules ){ 
     super(name);
     this.executor = exe;
     this.modules = modules;
@@ -25,7 +33,33 @@ public class Script extends Element implements Runnable{
       e.evaluate();
   }
   
-  public static Script nextScript(Reader reader, Executor exe){
-  return null;
+  public static Script nextScript( Reader reader, Executor exe ) throws ParseException, IOException {
+    //every script must start with the following: 
+    // ~<ScriptName>~  
+    String name = nextScriptName( reader );
+    List<Evaluable> instructions = new ArrayList<>();
+    Set<Module> modules = new HashSet<>();
+    
+  return new Script( name, instructions, exe, modules );
   }  
+
+  private static String nextScriptName( Reader r ) throws ParseException, IOException {
+    BufferedReader reader = new BufferedReader(r);
+    String line;
+
+    //skip through all empty lines
+    do {
+      line = StringUtils.trimToEmpty( reader.readLine() );
+    } while ( line.isEmpty()); 
+
+
+    int nameBegin = 2;
+    int nameEnd = line.indexOf(">~");
+
+    if( !line.startsWith("~<") || !line.endsWith(">~") ){
+      throw new ParseException("Each script must begin with a script name specified by ~<scriptName>~", 0);
+    }
+
+  return line.substring(nameBegin, nameEnd); 
+  }
 }
